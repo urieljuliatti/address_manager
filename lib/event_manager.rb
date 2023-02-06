c26a132829c9cb9b594edac431a8c9cc7f227c22
Uriel Juliatti Valle <uriel.juliattivalle@gmail.com>
2023-02-06 17:04:58 -0300

    Improving the code with CSV class and lib

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,13 +1,10 @@
+require 'csv'
+
 puts 'EventManager initialized.'
 
-# read the lines
-lines = File.readlines('event_attendees.csv')
-
-lines.each_with_index do |line,index|
-  # skipping the header
-  next if index.zero?
-
-  columns = line.split(',')
-  name = columns[2]
+# headers: true skips the header
+contents = CSV.open('event_attendees.csv', headers: true)
+contents.each do |row|
+  name = row[2]
   puts name
 end
