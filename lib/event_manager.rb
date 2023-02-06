6d8d47643145908b385aa57776ddd5d99fbb038a
Uriel Juliatti Valle <uriel.juliattivalle@gmail.com>
2023-02-06 13:45:27 -0300

    separate the columns into an array

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -2,5 +2,7 @@ puts 'EventManager initialized.'
 
 lines = File.readlines('event_attendees.csv')
 lines.each do |line|
-  puts line
+  # separate the columns into an array
+  columns = line.split(',')
+  p columns
 end
