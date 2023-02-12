cf8d40d12b6a87330d283fce1e5e70f078e8c916
Uriel Juliatti Valle <uriel.juliattivalle@gmail.com>
2023-02-12 10:40:27 -0300

    Cleaning up if and else statements to one line

--- lib/modules/clean_zipcode.rb
+++ lib/modules/clean_zipcode.rb
@@ -1,12 +1,5 @@
 module CleanZipcode
   def clean(zipcode)
-    if zipcode.nil?
-      zipcode = "00000"
-    elsif zipcode.length < 5
-      zipcode = zipcode.rjust(5, '0')
-    elsif zipcode.length > 5
-      zipcode = zipcode[0..4]
-    end
-    zipcode
+    zipcode.to_s.rjust(5, '0')[0..4]
   end
 end
